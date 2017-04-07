ff529594cc094ec335fe9593208b9c914642878c
Laurence Keith Albano <laurence.albano18@gmail.com>
2017-04-07 14:49:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
 	#nav-menu nav {border-radius: 20px 0px 20px 0px;background-color: rgba(255, 255, 255, 0.9); border:2px solid black;}
 	#nav-menu nav ul {margin:0;padding:0;}
 	#nav-menu nav ul li{list-style-type:none; padding: 0px 10px 5px 10px;}
-	#nav-menu nav ul li a{display: block; padding:10px 10px; color: green;transition: all 0.3s linear;}
-	#nav-menu nav ul li a:hover, #nav-menu nav ul li.current_page_item > a{color: #fff;background: linear-gradient(#09f7d7, #124706);border-radius: 10px;}
+	#nav-menu nav ul li a{display: block; padding:10px 10px; color: black;transition: all 0.3s linear;background: linear-gradient(#000000, #ffffff);border-radius: 10px;}
+	#nav-menu nav ul li a:hover, #nav-menu nav ul li.current_page_item > a{color: #fff;background: linear-gradient(#09f7d7, #124706);}
 
 	/* MAIN CONFIG */ 
 	.main-title {border-top: 1px solid black; border-bottom: 1px solid black; margin: 10px 0 10px 0;}
@@ -26,14 +26,6 @@
 	main:before {content:'';position: absolute; margin: 20px -29px; border: 10px solid transparent;border-right-color: white;}
 
 	/* MAIN-TABLE CONFIG */
-	.main-table {display:inline-block;}
-
-	/* ASIDE RIGHT CONFIG */
-	#aside-right {width:250px; height:auto; background:white; border-radius: 10px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}
-	.aside-right-content {padding: 0 10px 0 10px; margin:0; auto;}
-	.aside-right-title {border-top: 1px solid black; border-bottom: 1px solid black;margin:10px 0 0 0;}
-
-	/* TABLE CONFIG */
 	#main-table {width: 100%; border-collapse: collapse;}
 	#main-table th {background: #4CAF50; color:white;}
 	#main-table th:first-child { border-radius: 10px 0 0 0;}
@@ -41,16 +33,22 @@
 	#main-table tr:nth-child(even){background-color: #f2f2f2}
 	#main-table td {text-align:left;}
 	#main-table td:hover{background-color:#e2eac9;}
+	
+	/* ASIDE RIGHT CONFIG */
+	#aside-right {width:250px; height:auto; background:white; border-radius: 10px 0 10px 0;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}
+	.aside-right-content {padding: 0 10px 0 10px; margin:0; auto;}
+	.aside-right-title {border-top: 1px solid black; border-bottom: 1px solid black;margin:10px 0 0 0;}
+
+
 
 	/* FOOTER CONFIG */
-	footer { padding: 5px 0; clear:both;text-align:center;height:50px; background:red; color:#fff;}
+	footer { padding: 5px 0; clear:both;text-align:center;height:50px; background:red; color:#fff; border-radius: 0 0 10px 10px;}
 	.copyright {padding: 10px;}
 	.copyrightlink {text-decoration:underline; color:#fff;}
 	</style>
 <body>
 
 <div class="container">
-
 	<!-- Aside Nav Menu Left Div -->
 	<aside id="nav-menu">
 		<div class="container">
