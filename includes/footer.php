7f532b46323b7379ecbd5bfa0468609be06af47c
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2017-04-08 14:40:20 +0800

    transfer all files in the root directiry

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<!-- Footer Div -->
+<footer>
+  <div class="container">
+  <div class="copyright">
+  Copyright &copy
+  <?php
+        $yearStart = '2017';
+        $yearCurrent = date('Y');
+        $copyright = $yearCurrent;
+        echo $yearStart, ' - ' ,$copyright;
+    ?> USJR Online Enrollment System : <a href="" class="copyrightlink">COM.E Club</a> : All Rights Reserved.
+  </div>
+  </div>
+</footer>
+</body>
+
+<script type="text/javascript" src="js/script-js.js"></script>
+</html>
---
+++ includes/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Online Enrollment System USJR</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
