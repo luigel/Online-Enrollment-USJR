26517d1d915f24805adc5a01071bc2e548e76bf7
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2017-04-10 21:09:42 +0800

    moved the includes folder in application/views

--- assets/includes/head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Online Enrollment System USJR</title>
-    <?php echo link_tag('assets/css/style.css'); ?>
-  </head>
-  <body>
