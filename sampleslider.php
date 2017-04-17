affda92958f3c6e7c093c33377c221c908a48f8d
Laurence Keith Albano <laurence.albano18@gmail.com>
2017-04-17 10:27:31 +0800

    Added sampleslider content
    for slider pictures

---
+++ sampleslider.php
@@ -0,0 +1,27 @@
+
+<div id="backgroundBanner">	
+	<div id = "banner" class="container">	
+		<ul class="rslides">
+			<li>
+				<img src="<?php bloginfo('template_url'); ?>/images/slides/slide1.jpg" alt="Slides"/>
+				<div class = "banner-text">
+					<h2 class="bannerTitle">WELCOME TO CAREONE HEALTHCARE</h2>
+					<h2><span class="bannercontext">We Make Living at Home Even Better</span></h2>
+					<div class="Learn">
+						<a href="#">LEARN MORE</a>
+					</div>	
+				</div>
+			</li>
+			<li>
+				<img src="<?php bloginfo('template_url'); ?>/images/slides/slide2.jpg" alt="Slides"/>
+				<div class = "banner-text" id="move">
+					<h2 class="bannerTitle">WELCOME TO CAREONE HEALTHCARE</h2>
+					<h2><span class="bannercontext">We Make Living at Home Even Better</span></h2>
+					<div class="Learn">
+						<a href="#">LEARN MORE</a>
+					</div>	
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
